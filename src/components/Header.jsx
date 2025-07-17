1cf39997175bc9529186df049f7db7ae74a63d3a
Pénélope PIGEAT <penelopepigeat@MacBook-Pro-de-Penelope.local>
2025-07-17 20:46:49 +0200

    pagination

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
+import banniere from "../img/banniere.jpg";
+
 const Header = () => {
   return (
-    <div>
+    <div className="header">
       <p>Logo</p>
       <button>s'inscrire</button>
       <button>Se connecter</button>
       <button>vends tes articles</button>
+      <img className="header-banner" src={banniere} alt="bannière vinted" />
     </div>
   );
 };
+
 export default Header;
