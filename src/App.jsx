735edb43774377f5f663eff4a73cb9705faaf905
Pénélope PIGEAT <penelopepigeat@MacBook-Pro-de-Penelope.local>
2025-07-18 12:01:55 +0200

    page signup modifications

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,25 @@ import Offer from "./pages/Offer";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Header from "./components/Header";
+import Cookie from "js-cookie";
 
 function App() {
+  const handleToken = (token) => {
+    if (token) {
+      Cookie.setItem("userToken", token, { expires: 7 });
+    } else {
+      Cookie.remove("userToken");
+    }
+  };
+
   return (
     <Router>
-      <Header />
+      <Header handleToken={handleToken} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/offer/:id" element={<Offer />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp handleToken={handleToken} />} />
+        <Route path="/login" element={<Login handleToken={handleToken} />} />
       </Routes>
     </Router>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,21 @@
+import logo from "../img/logo.svg";
 import banniere from "../img/banniere.jpg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      <p>Logo</p>
-      <button>s'inscrire</button>
-      <button>Se connecter</button>
-      <button>vends tes articles</button>
-      <button>déconnexion</button>
+      <Link to="/">
+        <img className="logo" src={logo} alt="logo" />
+      </Link>
+      <Link to="/signup">
+        <button>S'inscrire</button>
+      </Link>
+      <Link to="/login">
+        <button>Se connecter</button>
+      </Link>
+      <button>Vends tes articles</button>
+      <button>Déconnexion</button>
       <img className="header-banner" src={banniere} alt="bannière vinted" />
     </div>
   );
